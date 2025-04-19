7c1e3d9aaa5e64084f5ce99d450b25d672918d0d
Forhad Ahsan <mdforhadahsan1999@gmail.com>
2025-04-19 11:42:55 +0600

    condition - class='weekends?bg-red-500':'bg-green-500'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
  const weater = 'r' // s, pc, r, c, ci
+
+ const weekends = false
 </script>
 
 <template>
@@ -27,6 +29,10 @@
     <p v-show="weater=='c'">Cloudy</p>
     <p v-show="weater=='ci'">Cyclone</p>
   </div>
+  <div>
+    <h2>Weekend </h2>
+    <p class="h-10 flex items-center justify-center" :class="weekends?'bg-red-500':'bg-green-400'">Weekend</p>
+  </div> 
 </template>
 
 <style scoped>
