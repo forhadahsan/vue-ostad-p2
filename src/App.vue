277b63aa5d9992414c6c308c3e4034e2a79a60bc
Forhad Ahsan <mdforhadahsan1999@gmail.com>
2025-04-19 11:00:52 +0600

    loop (if & else)

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,14 @@
     <p v-else-if="weater=='c'">cloud</p>
     <p v-else-if="weater=='ci'">cyclone</p>
   </div>
+  <div>
+    <h2>Weather Loop (v-if & v-else-if)</h2>
+    <p v-if="weater=='s'">Sunny</p>
+    <p v-else-if="weater=='pc'">Partially Cloudy</p>
+    <p v-else-if="weater=='r'">Rain</p>
+    <p v-else-if="weater=='c'">Cloudy</p>
+    <p v-else-if="weater=='ci'">Cyclone</p>
+  </div>
 </template>
 
 <style scoped>
