6f7538e31d1c3a6c0f4922b8eab57efabb2a3b67
Forhad Ahsan <mdforhadahsan1999@gmail.com>
2025-04-19 10:58:04 +0600

    start

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+ const weater = 'r' // s, pc, r, c, ci
+</script>
+
+<template>
+  <div>
+    <h2>Hello Vue</h2>
+    <p v-if="weater=='s'">Sunny</p>
+    <p v-else-if="weater=='pc'">partials cloud</p>
+    <p v-else-if="weater=='r'">Rain</p>
+    <p v-else-if="weater=='c'">cloud</p>
+    <p v-else-if="weater=='ci'">cyclone</p>
+  </div>
+</template>
+
+<style scoped>
+ 
+</style>
